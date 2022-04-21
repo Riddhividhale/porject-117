13092e75793407ec9d61b95d2c226b1914bbe24a
Riddhividhale <87593958+Riddhividhale@users.noreply.github.com>
2022-04-21 20:46:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    canvas=creatcanvas(280,280);
+    canavs.center();
+    background('white');
+    }
+    
+    
+    function clearcanvas() {
+        background('white');
+    }
